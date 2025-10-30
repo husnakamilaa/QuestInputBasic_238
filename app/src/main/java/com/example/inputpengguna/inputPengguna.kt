1729cd439fdcaed584571f6aad9cfb643016a20e
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-30 09:00:38 +0700

    feat: membuat komponen text edit dan mendeklarasikan komponen value dengan var textName

--- app/src/main/java/com/example/inputpengguna/inputPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/inputPengguna.kt
@@ -3,6 +3,7 @@ package com.example.inputpengguna
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,5 +27,9 @@ fun FormDataDiri(modifier: Modifier) {
     Column(modifier= modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally)
-        {  }
+        {
+            OutlinedTextField(
+                value = textName,
+            )
+        }
 }
