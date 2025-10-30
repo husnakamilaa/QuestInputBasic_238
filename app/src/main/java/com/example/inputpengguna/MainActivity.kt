88165cf854a536c0774877227c17f348c144df58
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-30 14:03:06 +0700

    feat: memanggil function FormDataDiri pada mainActivity

--- app/src/main/java/com/example/inputpengguna/MainActivity.kt
+++ app/src/main/java/com/example/inputpengguna/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             InputPenggunaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    FormDataDiri(
+                        modifier = Modifier.padding(paddingValues = innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InputPenggunaTheme {
-        Greeting("Android")
     }
 }
