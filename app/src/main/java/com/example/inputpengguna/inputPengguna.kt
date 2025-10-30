7b798de8ee46047513480d7217e9ebaa1970bbf5
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-30 09:20:11 +0700

    fix: memperbaiki beberapa komponen atau layout yang error

--- app/src/main/java/com/example/inputpengguna/inputPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/inputPengguna.kt
@@ -2,6 +2,7 @@ package com.example.inputpengguna
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
@@ -12,14 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
-import java.lang.reflect.Modifier
 
 @Composable
-fun FormDataDiri(modifier: Modifier) {
+fun FormDataDiri(modifier: Modifier, it: Any) {
     var textName by remember { mutableStateOf(value="") }
     var textAlamat by remember { mutableStateOf(value="") }
     var textJK by remember { mutableStateOf(value = "") }
@@ -49,7 +49,7 @@ fun FormDataDiri(modifier: Modifier) {
                     Row(modifier = Modifier.selectable(
                         selected = textJK == item,
                         onClick = {textJK = item}
-                    ), verticalArrangement = Alignment.CenterVertically) {
+                    ), verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
                             selected = textJK == item,
                             onClick = {
