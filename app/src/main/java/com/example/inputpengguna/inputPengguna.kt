75bd49b5981e064e21ea377d0e6533ad5bafa6d0
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-30 08:58:15 +0700

    feat: menambahkan properties layout utama (column) dengan penataan atas secara vertikal dan tengah secara horizontal

--- app/src/main/java/com/example/inputpengguna/inputPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/inputPengguna.kt
@@ -1,10 +1,12 @@
 package com.example.inputpengguna
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
@@ -22,5 +24,7 @@ fun FormDataDiri(modifier: Modifier) {
     val gender: List<String> = listOf("Laki-laki", "Perempuan")
 
     Column(modifier= modifier.padding(top = 50.dp),
-        ) {  }
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally)
+        {  }
 }
