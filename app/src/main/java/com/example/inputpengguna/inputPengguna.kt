6a90c50589ed4f43d7c9991b52d2cdac3890c611
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-30 09:08:32 +0700

    feat: mendeklarasikan komponen onValueChange (saat mengetik variable) dengan var textName

--- app/src/main/java/com/example/inputpengguna/inputPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/inputPengguna.kt
@@ -38,8 +38,9 @@ fun FormDataDiri(modifier: Modifier) {
                 shape = MaterialTheme.shapes.large,
                 modifier = Modifier.width(width = 250.dp),
                 label = { Text(text =  "Nama Lengkap") },
-
-
+                onValueChange =  {
+                    textName = it
+                }
             )
         }
 }
