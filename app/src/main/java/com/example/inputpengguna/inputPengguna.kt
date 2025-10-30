c60a23b924261e16f8901f5e500a7736f1b711dc
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-30 08:50:22 +0700

    feat: mendeklarasikan variable mutable untuk textfield 'saat masih mengetik'

--- app/src/main/java/com/example/inputpengguna/inputPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/inputPengguna.kt
@@ -1,9 +1,16 @@
 package com.example.inputpengguna
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import java.lang.reflect.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier) {
+    var textName by remember { mutableStateOf(value="") }
+    var textAlamat by remember { mutableStateOf(value="") }
+    var textJK by remember { mutableStateOf(value = "") }
+
+
 
 }
